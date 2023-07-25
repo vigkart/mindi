abbf3c91ea5293e47d7513031e76ad4347d7c075
Vignesh Karthik <124303271+vigkart@users.noreply.github.com>
2023-07-25 16:09:23 -0500

    mindi

---
+++ janitor.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import os
+from scipy.stats import zscore
+
+# Define the directory paths
+raw_data_dir = r'C:\Users\vkarthik\Documents\vscode\mindi\trading_data\raw_data'
+clean_data_dir = r'C:\Users\vkarthik\Documents\vscode\mindi\trading_data\clean_data'
+outliers_dir = r'C:\Users\vkarthik\Documents\vscode\mindi\trading_data\outliers'
+
+# Create the output directories if they don't exist
+os.makedirs(clean_data_dir, exist_ok=True)
+os.makedirs(outliers_dir, exist_ok=True)
+
+import pandas as pd
+import os
+from scipy.stats import zscore
+
+# Define the directory paths
+raw_data_dir = r'C:\Users\vkarthik\Documents\vscode\mindi\trading_data\raw_data'
+clean_data_dir = r'C:\Users\vkarthik\Documents\vscode\mindi\trading_data\clean_data'
+outliers_dir = r'C:\Users\vkarthik\Documents\vscode\mindi\trading_data\outliers'
+
+# Create the output directories if they don't exist
+os.makedirs(clean_data_dir, exist_ok=True)
+os.makedirs(outliers_dir, exist_ok=True)
+
+# Iterate over all CSV files in the raw data directory
+for filename in os.listdir(raw_data_dir):
+    if filename.endswith('_matches.csv'):
+        # Extract the algorithm number from the filename
+        algo_number = filename.split('_')[0]
+
+        # Load the data
+        data_path = os.path.join(raw_data_dir, filename)
+        data = pd.read_csv(data_path)
+        
+        # Remove rows where 'avg_entry_px' or 'avg_exit_px' is 0
+        data = data[(data['avg_entry_px'] != 0) | (data['avg_exit_px'] != 0)]
+        
+        # Calculate the ratio and z-scores for avg_entry_px / avg_exit_px and pnl
+        data['price_ratio'] = data['avg_entry_px'] / data['avg_exit_px']
+        data['price_ratio_z_score'] = zscore(data['price_ratio'])
+        data['pnl_z_score'] = zscore(data['pnl'])
+
+        # Create the clean dataset by removing the outliers in both the price ratio and pnl
+        clean_data = data[(abs(data['price_ratio_z_score']) <= 3) & (abs(data['pnl_z_score']) <= 3)]
+
+        # Identify the outliers
+        outliers = data[(abs(data['price_ratio_z_score']) > 3) & (abs(data['pnl_z_score']) > 3)]
+
+        # Save the clean data to a CSV file
+        clean_data_path = os.path.join(clean_data_dir, f'{algo_number}_clean.csv')
+        clean_data.to_csv(clean_data_path, index=False)
+
+        # Save the outliers to a separate CSV file
+        outliers_path = os.path.join(outliers_dir, f'{algo_number}_outliers.csv')
+        outliers.to_csv(outliers_path, index=False)
+
+
+
