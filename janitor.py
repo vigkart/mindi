5052b2ae965a2de1e352eb0347b14c85801da702
vigkart <vk2504@columbia.edu>
2023-08-16 02:47:09 -0500

    fixed infinite numbers bug by deleting the rows

--- janitor.py
+++ janitor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from scipy.stats import zscore
+import numpy as np
 
 # Define the directory paths
 raw_data_dir = r'trading_data/raw_data'
@@ -17,10 +18,19 @@ for filename in os.listdir(raw_data_dir):
         # Extract the algorithm number from the filename
         algo_number = filename.split('_')[0]
 
+        # Creating output paths
+        clean_data_path = os.path.join(clean_data_dir, f'{algo_number}_clean.csv')
+        outliers_path = os.path.join(outliers_dir, f'{algo_number}_outliers.csv')
+
+        # If file has already been cleaned, skip
+        if os.path.isfile(clean_data_path):
+            print(f'Algo number {algo_number} has been processed already')
+            continue
+
         # Load the data
         data_path = os.path.join(raw_data_dir, filename)
-        data = pd.read_csv(data_path)
-        
+        data = pd.read_csv(data_path, keep_default_na=False)
+
         # Checking if turd to decide limit for cutting low value, unclean trades
         turd = [2, 7, 8 , 9 , 10 , 11, 23]
         if int(algo_number) in turd:
@@ -29,9 +39,20 @@ for filename in os.listdir(raw_data_dir):
         else:
             # Remove rows where 'avg_entry_px' or 'avg_exit_px' is < 5 for non-turd strategies
             data = data[(data['avg_entry_px'] > 5) | (data['avg_exit_px'] > 5)]
+
+
         
-        # Calculate the ratio and z-scores for avg_entry_px / avg_exit_px and pnl
+        # Calculate the price ratio
         data['price_ratio'] = data['avg_entry_px'] / data['avg_exit_px']
+
+        # Removing rows with any infinite values (which can be introduced in previous step)
+        numeric_columns = data.select_dtypes(include=[float, int]).columns
+        infinite_rows = data[data[numeric_columns].applymap(np.isinf).any(axis=1)] # infinite values are being squashed here (done after calculating price ratio because of potentially introduced inf's)
+        data = data[~data[numeric_columns].applymap(np.isinf).any(axis=1)]
+        
+        # Calculating z-scores for avg_entry_px / avg_exit_px and pnl
+        if data['price_ratio'].isna().any():
+            print("NaN values introduced in price_ratio.")
         data['price_ratio_z_score'] = zscore(data['price_ratio'])
         data['pnl_z_score'] = zscore(data['pnl'])
 
@@ -40,11 +61,11 @@ for filename in os.listdir(raw_data_dir):
 
         # Identify the outliers
         outliers = data[(abs(data['price_ratio_z_score']) > 3) & (abs(data['pnl_z_score']) > 3)]
+        outliers = pd.concat([outliers, infinite_rows], ignore_index=True)
 
         # Save the clean data to a CSV file
-        clean_data_path = os.path.join(clean_data_dir, f'{algo_number}_clean.csv')
         clean_data.to_csv(clean_data_path, index=False)
 
         # Save the outliers to a separate CSV file
-        outliers_path = os.path.join(outliers_dir, f'{algo_number}_outliers.csv')
         outliers.to_csv(outliers_path, index=False)
+        print(f"Cleaned {filename}")
