2d503c4fd9aec94035b915e2a26ffd10b60c49ee
vigkart <vk2504@columbia.edu>
2023-08-27 17:58:30 -0400

    Classifier and adjusted data building

--- classifier.py
+++ classifier.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import os
+from scipy.spatial import distance
 
 # Getting the data
 classification_data_dir = r"classification_data"
@@ -17,40 +17,96 @@ X_test = pd.read_csv(X_test_path, index_col=0)
 y_train = pd.read_csv(y_train_path, index_col=0)
 y_test = pd.read_csv(y_test_path, index_col=0)
 
+assert not np.any(np.isnan(X_train))
+assert not np.any(np.isnan(y_train))
+
+# Scaling layer shapes for given features and algorithms
+nfeatures = X_train.shape[1]
+nalgos = y_train.shape[1]
+layer1_nodes = int(nfeatures/2)
+layer2_nodes = int(layer1_nodes/2)
+layer3_nodes = int(layer2_nodes/2)
+
+np.random.seed(42)
+tf.random.set_seed(42)
+# initializer = tf.keras.initializers.HeNormal()
+tf.keras.layers.Layer.default_kernel_initializer = tf.keras.initializers.HeNormal()
+
 # Build the model
-model = tf.keras.Sequential(
-    [
-        tf.keras.layers.Dense(90, activation="tanh"),
-        tf.keras.layers.Dense(60, activation="elu"),
-        tf.keras.layers.Dense(30, activation="elu"),
-        tf.keras.layers.Dense(15, activation="relu"),
-        tf.keras.layers.Dense(3, activation="sigmoid"),
+model = tf.keras.Sequential([
+        tf.keras.layers.Dense(nfeatures, input_shape=(nfeatures,)),  # Define input shape for the first layer
+        tf.keras.layers.BatchNormalization(),  # Batch normalization layer for the input
+        tf.keras.layers.Dropout(.5),
+        tf.keras.layers.Dense(layer1_nodes, activation="relu"),
+        tf.keras.layers.Dropout(.5),
+        tf.keras.layers.Dense(layer2_nodes, activation="relu"),
+        tf.keras.layers.Dense(layer3_nodes, activation="relu"),
+        tf.keras.layers.Dense(nalgos, activation="sigmoid")
     ]
 )
 
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, clipvalue=1.0)
 model.compile(
-    optimizer="adam",
-    loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
+    optimizer=optimizer,
+    # loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
+    loss=tf.keras.losses.BinaryCrossentropy(),
     metrics=["accuracy"],
 )
 
-model.fit(X_train, y_train, epochs=100)
+model.fit(X_train, y_train, epochs=64)
 
 # Make the predictions
 predictions = model.predict(X_test)
 threshold = 0.5
+predictions = pd.DataFrame(data=predictions, index=y_test.index, columns=y_test.columns)
+threshold = 0.5
 binary_predictions = (predictions > threshold).astype(int)
+binary_predictions = binary_predictions
+# binary_predictions = pd.DataFrame(binary_predictions, columns=y_test.columns)
+y_test = y_test.astype(int)
 
-predictions = pd.DataFrame(data=predictions, index=y_test.index)
-binary_predictions = (predictions > 0.5).astype(float)
 print(predictions.head(15))
 print(binary_predictions.head(15))
-
-# for i, pred in enumerate(predictions):
-#     print(f'Sample {i+1}: {pred}')
-
-# for i, pred in enumerate(binary_predictions):
-#     print(f'Sample {i+1}: {pred}')
+print(y_test.head(15))
 
 
-test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
+test_loss, test_acc = model.evaluate(X_test, y_test, verbose=1)
+
+
+predictable = []
+not_predictable = []
+ydrop_cols = []
+
+for column in y_test.columns:
+    corr = 1 - distance.jaccard(y_test[column], binary_predictions[column]) # jaccard of 0 is good, so we are doing 1 - jaccard to make it more intuitive
+    print(f'{column}: {round(corr, 3)}')
+    if corr > 0.5:
+        predictable.append(column)
+    else:
+        ydrop_cols.append(column)
+        not_predictable.append(column.split('_')[0])
+
+print(f"These algorithms are reliably predictable: {predictable}")
+
+# Removing noise (unpredictable algorithms) from training and test sets
+Xdrop_cols = []
+for col in X_train.columns:
+    if col.split('_')[0] in not_predictable:
+        Xdrop_cols.append(col)
+
+
+X2_train = X_train.drop(labels=Xdrop_cols, axis=1)
+X2_test = X_test.drop(labels=Xdrop_cols, axis=1)
+y2_train = y_train.drop(labels=ydrop_cols, axis=1)
+y2_test = y_test.drop(labels=ydrop_cols, axis=1)
+
+
+X2_train_path = os.path.join(classification_data_dir, "x2_train.csv")
+X2_test_path = os.path.join(classification_data_dir, "x2_test.csv")
+y2_train_path = os.path.join(classification_data_dir, "y2_train.csv")
+y2_test_path = os.path.join(classification_data_dir, "y2_test.csv")
+
+X2_train.to_csv(X2_train_path, index=True, float_format="%.6f")
+X2_test.to_csv(X2_test_path, index=True, float_format="%.6f")
+y2_train.to_csv(y2_train_path, index=True)
+y2_test.to_csv(y2_test_path, index=True)
--- data_builder.py
+++ data_builder.py
@@ -1,13 +1,18 @@
 import os
 import pandas as pd
-from sklearn.preprocessing import MaxAbsScaler
+from sklearn.preprocessing import MinMaxScaler
 from joblib import dump
 from sklearn.model_selection import train_test_split
 
 # Which algos would you like to consider?
-algo_list = [2, 35, 46]
 features_data_dir = r"features_data"
+features_files = os.listdir(features_data_dir)
+algo_list = []
+for i in range(len(features_files)):
+    num = features_files[i].split('_')[0]
+    algo_list.append(num)
 
+print(algo_list)
 
 def read_features_csv(algo_num):
     filename = str(algo_num) + "_features.csv"
@@ -23,9 +28,16 @@ X_init, y_init = read_features_csv(algo_list[0])
 
 for algo_num in algo_list[1:]:
     features_df, pnl = read_features_csv(algo_num)
-    X_init = X_init.join(features_df, how="inner")
-    y_init = y_init.join(pnl, how="inner")
+    X_init = X_init.join(features_df, how="outer")
+    y_init = y_init.join(pnl, how="outer")
 
+# print(X_init.shape)
+# X_init = X_init.dropna()
+# print(X_init.shape)
+
+# print(y_init.shape)
+# y_init = y_init.dropna()
+# print(y_init.shape)
 
 # Creating ground truth vectors (any profit with a zscore higher than 1, computed only on winning days)
 y_classified = y_init.copy()
@@ -35,20 +47,23 @@ for column in y_init.columns:
     series = winning_days[column].dropna()
     std = series.std()
     mean = series.mean()
-    required_value = std + mean
+    required_value = 0
 
-    y_classified.loc[y_classified[column] < required_value, column] = 0
-    y_classified.loc[y_classified[column] >= required_value, column] = 1
+    y_classified.loc[y_classified[column] <= required_value, column] = 0
+    y_classified.loc[y_classified[column] > required_value, column] = 1
+
+X_init = X_init.fillna(0)
+y_init = y_init.fillna(0)
 
 
 # Splitting into Training and Test sets
 X_train, X_test, y_train, y_test = train_test_split(
-    X_init, y_classified, test_size=0.2, random_state=42
+    X_init, y_classified, test_size=0.2, shuffle=False
 )
 
 
 # Normalizing the data
-scaler = MaxAbsScaler()
+scaler = MinMaxScaler()
 scaler.fit(X_train)
 X_train_scaled = scaler.transform(X_train)
 X_train = pd.DataFrame(X_train_scaled, index=X_train.index, columns=X_train.columns)
@@ -72,7 +87,15 @@ X_test_path = os.path.join(classification_data_dir, "x_test.csv")
 y_train_path = os.path.join(classification_data_dir, "y_train.csv")
 y_test_path = os.path.join(classification_data_dir, "y_test.csv")
 
+X_train = X_train.fillna(0)
+X_test = X_test.fillna(0)
+y_train = y_train.fillna(0)
+y_test = y_test.fillna(0)
+
 X_train.to_csv(X_train_path, index=True, float_format="%.6f")
 X_test.to_csv(X_test_path, index=True, float_format="%.6f")
 y_train.to_csv(y_train_path, index=True)
 y_test.to_csv(y_test_path, index=True)
+
+print(f"X_train.shape: {X_train.shape}")
+print(f"y_train.shape: {y_train.shape}")
